c95ce3d1bc7844c2494e0894a847e1f30bde092b
Isabelle Bicudo <isabellebicudo70@gmail.com>
2022-12-06 21:35:27 -0400

    first commit

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width:100%;
+    height:100vh;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+export const LoginContainer = styled.div`
+    width:100%;
+    max-width:300px;
+    background-color: #303030;
+
+    border-radius: 5px;
+    padding:50px;
+    margin:50px;
+
+    display: flex;
+    justify-content:center;
+    align-items:center;
+`
+
+export const Title = styled.h2`
+    font-size:30px;
+    font-weight:700;
+    color:#c4c3ca;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+export const Column = styled.div`
+    width:100%;
+    display:flex;
+    flex-direction:column;
+    align-items:flex-start;
+    justify-content: center;
+`
+
+export const Spacing =styled.div`
+    margin-top:10px;
+`
